e3ab151549bac1a5f517d17c3a23f9518f6ca808
IMoriana3 <imoriana3@gmail.com>
2026-03-27 16:07:17 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = 'checklist-solar-v3';
-const urlsToCache = ['./', './index.html', './master.js', './manifest.json'];
+const CACHE_NAME = 'checklist-solar-v6';
+const urlsToCache = ['./', './index.html', './master.js', './master_con.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(names => Promise.all(
-    names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-  )).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.url.includes('script.google.com') || e.request.url.includes('googleapis.com')) return;
   e.respondWith(
     fetch(e.request).then(r => {
-      const clone = r.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      if (r && r.status === 200) {
+        const clone = r.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      }
       return r;
     }).catch(() => caches.match(e.request))
   );
